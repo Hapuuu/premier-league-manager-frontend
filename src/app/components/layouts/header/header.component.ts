cf9578a88d5d6e5644a4881c226b0a0b23803099
hapu <thungu.hapuarachchi@gmail.com>
2020-12-30 00:22:06 +0530

    components refresh when generating match

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { TableDataService } from 'src/app/services/table-data.service';
-import { LeagueTableComponent } from '../../pages/league-table/league-table.component';
-import { MatchTableComponent } from '../../pages/match-table/match-table.component';
 
 @Component({
   selector: 'app-header',
@@ -10,13 +9,30 @@ import { MatchTableComponent } from '../../pages/match-table/match-table.compone
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private matchRequest:TableDataService) { }
+  constructor(private matchRequest:TableDataService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
   generateRandomMatch() {
+    // save current route 
+    const currentPage = this.router.url;
     this.matchRequest.requestRandomMatch().subscribe();
+
+    // checks if the current route is "/" and then navigate to that or else navigate to matchTable route
+    if (currentPage=="/") {
+      this.router.navigateByUrl('/matchTable', { skipLocationChange: true }).then(() => {
+        this.router.navigate([currentPage]); // navigate to same route which is "/"
+      }); 
+    } else {
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigate([currentPage]); // navigate to same route which is "/matchTable"
+      }); 
+    }
+
+
+
+
   }
 
 }
